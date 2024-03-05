1676e5c7ba0ce3ba823cc72f27bf6f84db752477
vikasgill640 <vikasgill640@gmail.com>
2024-03-05 12:22:36 +0530

    change backend file

--- s3_task/provider.tf
+++ s3_task/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "test-teraformstate-bucket"
+    bucket         = "test-teraformstate-bucket1"
     key            = "s3/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "teraform_lockId"
